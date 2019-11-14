baea348de9d3dca68aa519e206fdae7ad29bcdac
Aluno <Aluno@CR4UP13>
2019-11-14 11:38:31 +0000

    olá

---
+++ POO_Ramalhao_2019_2020/src/ramalhao/pgo/program/Linha.java
@@ -0,0 +1,19 @@
+package ramalhao.pgo.program;
+
+import java.awt.Polygon;
+
+public class Linha 
+{
+	public Polygon getPolygon() {
+		
+		
+		Polygon p = new Polygon(); 
+		 p.addPoint(100,151);
+		 p.addPoint(100,151);
+		 p.addPoint(100,151);
+		 p.addPoint(100,151);
+		 return p;
+     }
+	
+}	
+
