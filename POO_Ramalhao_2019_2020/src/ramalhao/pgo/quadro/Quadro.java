f657bff113c830201d8e7b45001283b8067c991d
vasco.p.costa <vasco.p.costa@CPX-HQDL8T55CJT.dir.svc.accenture.com>
2019-11-21 00:01:10 +0000

    versao 2 do exercicio - 20191121

--- POO_Ramalhao_2019_2020/src/ramalhao/pgo/quadro/Quadro.java
+++ POO_Ramalhao_2019_2020/src/ramalhao/pgo/quadro/Quadro.java
@@ -11,15 +11,20 @@ import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
+import ramalhao.pgo.quadro.figuras.Pentagono;
+import ramalhao.pgo.quadro.figuras.Point;
+import ramalhao.pgo.quadro.figuras.Quadrilatero;
+import ramalhao.pgo.quadro.figuras.Triangulo;
+
 public class Quadro extends JPanel {
 	
 	private static final String QUADRADO = "Quadrado";
     private static final String TRIANGULO = "Triangulo";
     private static final String RECTANGULO = "Rectangulo";
-    private static final String LINHA = "Linha";
     private static final String CASA = "Casa";
+    private static final String PENTAGONO_EQUIL = "Pentagono Equilátero";
     
-    private static final String[] FORMAS = { QUADRADO, TRIANGULO, RECTANGULO, LINHA, CASA };
+    private static final String[] FORMAS = { QUADRADO, TRIANGULO, RECTANGULO, CASA, PENTAGONO_EQUIL };
     
     private String textoForma = "";
     
@@ -50,38 +55,77 @@ public class Quadro extends JPanel {
 		super.paintComponent(g);
 		setBackground(Color.WHITE);
 		
-
-		if (TRIANGULO.equals(textoForma)){
+		if (TRIANGULO.equals(textoForma))
+		{
 			Graphics2D g2d = (Graphics2D) g.create();
 			g2d.setColor(Color.green);
-			Triangulo t = new Triangulo();
+			
+			Point ponto1 = new Point(300,300);
+			Point ponto2 = new Point(600,300);
+			Point ponto3 = new Point(450,500);
+			
+			Triangulo t = new Triangulo(ponto1, ponto2, ponto3);
 			g2d.fill(t.getPolygon());
 		}
-		else if (RECTANGULO.equals(textoForma)) {
+		else if (RECTANGULO.equals(textoForma)) 
+		{
 			Graphics2D g2d = (Graphics2D) g.create();
-			g.setColor(Color.RED);
-			Retangulo r = new Retangulo();
-			g2d.fill(r.getPolygon());
-		}	
-		else if (LINHA.equals(textoForma)) {
-			Graphics2D g2d = (Graphics2D) g.create();
-			g.setColor(Color.darkGray);
-			Linha l = new Linha();
-			g2d.fill(l.getPolygon());
-		}	
-		else if (QUADRADO.equals(textoForma)) {
-			Graphics2D g2d = (Graphics2D) g.create();
-			g.setColor(Color.RED);
-			quadrado q = new quadrado();
+			g2d.setColor(Color.yellow);
+
+			Point ponto1 = new Point(150, 150);
+			Point ponto2 = new Point(300, 150);
+			Point ponto3 = new Point(300, 500);
+			Point ponto4 = new Point(150, 500);
+			
+			Quadrilatero q = new Quadrilatero(ponto1, ponto2, ponto3, 
+					ponto4);
+			
 			g2d.fill(q.getPolygon());
 		}	
-		else if (CASA.equals(textoForma)) {
+		else if (QUADRADO.equals(textoForma)) 
+		{
 			Graphics2D g2d = (Graphics2D) g.create();
-			g.setColor(Color.RED);
-			casa c = new casa();
-			g2d.fill(c.getPolygon());
+			g2d.setColor(Color.RED);
+			
+			Point ponto1 = new Point(150, 150);
+			Point ponto2 = new Point(300, 150);
+			Point ponto3 = new Point(300, 300);
+			Point ponto4 = new Point(150, 300);
+			
+			Quadrilatero q = new Quadrilatero(ponto1, ponto2, ponto3, 
+					ponto4);
+			g2d.fill(q.getPolygon());
 		}	
-
+		else if (CASA.equals(textoForma)) 
+		{
+			Graphics2D g2d = (Graphics2D) g.create();
+			g2d.setColor(Color.gray);
+			
+			Point ponto1 = new Point(150, 150);
+			Point ponto2 = new Point(225, 50);
+			Point ponto3 = new Point(300, 150);
+			Point ponto4 = new Point(300, 300);
+			Point ponto5 = new Point(150, 300);
+			
+			Pentagono c = new Pentagono(ponto1, ponto2, ponto3, 
+					ponto4, ponto5);
+			g2d.fill(c.getPolygon());
+		}
+		else if(PENTAGONO_EQUIL.equals(textoForma)) 
+		{
+			Graphics2D g2d = (Graphics2D) g.create();
+			g2d.setColor(Color.BLUE);
+			
+			Point ponto1 = new Point(350, 150);
+			Point ponto2 = new Point(255, 219);
+			Point ponto3 = new Point(291, 331);
+			Point ponto4 = new Point(409, 331);
+			Point ponto5 = new Point(445, 219);
+			
+			Pentagono c = new Pentagono(ponto1, ponto2, ponto3, 
+					ponto4, ponto5);
+			g2d.fill(c.getPolygon());
+		}
 	}
 	
 	class RadioBtnListenerFormas implements ActionListener {
---
+++ POO_Ramalhao_2019_2020/src/ramalhao/pgo/quadro/figuras/Triangulo.java
@@ -0,0 +1,28 @@
+package ramalhao.pgo.quadro.figuras;
+
+import java.awt.Polygon;
+
+public class Triangulo
+{
+	private Point ponto1;
+	private Point ponto2;
+	private Point ponto3;
+	
+	public Triangulo(Point ponto1, Point ponto2, Point ponto3) 
+	{
+		this.ponto1 = ponto1;
+		this.ponto2 = ponto2;
+		this.ponto3 = ponto3;
+	}
+	
+	public Polygon getPolygon()
+	{
+		Polygon p= new Polygon();
+		
+		p.addPoint(ponto1.getX(), ponto1.getY());
+		p.addPoint(ponto2.getX(), ponto2.getY());
+		p.addPoint(ponto3.getX(), ponto3.getY());
+		
+		return p;
+	}
+}
