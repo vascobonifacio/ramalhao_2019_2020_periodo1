00164a8a3dda4f6a01d4e2c257ec16b3c05aadce
Aluno <Aluno@CR4UP12.mshome.net>
2019-11-21 11:58:33 +0000

    Trabalho Pedro

---
+++ POO_Ramalhao_2019_2020/src/ramalhao/pgo/quadro/figuras/Triangulo.java
@@ -0,0 +1,56 @@
+package ramalhao.pgo.quadro.figuras;
+
+import java.awt.Polygon;
+
+public class Triangulo {
+	private Point ponto1;
+	private Point ponto2;
+	private Point ponto3;
+
+	public Triangulo (Point ponto1, Point ponto2, Point ponto3 ) {
+
+		super();
+		this.ponto1= ponto1;
+		this.ponto2= ponto2;
+		this.ponto3= ponto3;
+	}
+
+	public Point getPonto1() {
+		return ponto1;
+	}
+
+	public void setPonto1(Point ponto1) {
+		this.ponto1 = ponto1;
+	}
+
+	public Point getPonto2() {
+		return ponto2;
+	}
+
+	public void setPonto2(Point ponto2) {
+		this.ponto2 = ponto2;
+	}
+
+	public Point getPonto3() {
+		return ponto3;
+	}
+
+	public void setPonto3(Point ponto3) {
+		this.ponto3 = ponto3;
+	}
+
+
+
+	  public Polygon getPolygon( ) {
+		  
+		  Polygon polygon= new Polygon();
+		  
+	  }
+
+
+
+
+
+
+
+}
