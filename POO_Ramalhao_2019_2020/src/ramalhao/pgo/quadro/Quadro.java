629e6719e24ef4fad02e6a64579f6a92f1cdadae
vasco.p.costa <vasco.p.costa@CPX-HQDL8T55CJT.dir.svc.accenture.com>
2019-11-21 00:09:25 +0000

    continuar exercicio - 20191121

--- POO_Ramalhao_2019_2020/src/ramalhao/pgo/quadro/Quadro.java
+++ POO_Ramalhao_2019_2020/src/ramalhao/pgo/quadro/Quadro.java
@@ -11,11 +11,6 @@ import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
-import ramalhao.pgo.quadro.figuras.Pentagono;
-import ramalhao.pgo.quadro.figuras.Point;
-import ramalhao.pgo.quadro.figuras.Quadrilatero;
-import ramalhao.pgo.quadro.figuras.Triangulo;
-
 public class Quadro extends JPanel {
 	
 	private static final String QUADRADO = "Quadrado";
@@ -60,71 +55,46 @@ public class Quadro extends JPanel {
 			Graphics2D g2d = (Graphics2D) g.create();
 			g2d.setColor(Color.green);
 			
-			Point ponto1 = new Point(300,300);
-			Point ponto2 = new Point(600,300);
-			Point ponto3 = new Point(450,500);
 			
-			Triangulo t = new Triangulo(ponto1, ponto2, ponto3);
-			g2d.fill(t.getPolygon());
+//			TODO inicializar pontos, criar triangulo numa variável chamada t, 
+//			depois descomentar a linha abaixo
+//			g2d.fill(t.getPolygon());
 		}
 		else if (RECTANGULO.equals(textoForma)) 
 		{
 			Graphics2D g2d = (Graphics2D) g.create();
 			g2d.setColor(Color.yellow);
-
-			Point ponto1 = new Point(150, 150);
-			Point ponto2 = new Point(300, 150);
-			Point ponto3 = new Point(300, 500);
-			Point ponto4 = new Point(150, 500);
 			
-			Quadrilatero q = new Quadrilatero(ponto1, ponto2, ponto3, 
-					ponto4);
-			
-			g2d.fill(q.getPolygon());
+//			TODO inicializar pontos, criar quadrilatero numa variável chamada q, 
+//			depois descomentar a linha abaixo
+//			g2d.fill(q.getPolygon());
 		}	
 		else if (QUADRADO.equals(textoForma)) 
 		{
 			Graphics2D g2d = (Graphics2D) g.create();
 			g2d.setColor(Color.RED);
 			
-			Point ponto1 = new Point(150, 150);
-			Point ponto2 = new Point(300, 150);
-			Point ponto3 = new Point(300, 300);
-			Point ponto4 = new Point(150, 300);
-			
-			Quadrilatero q = new Quadrilatero(ponto1, ponto2, ponto3, 
-					ponto4);
-			g2d.fill(q.getPolygon());
+//			TODO inicializar pontos, criar quadrilatero numa variável chamada q, 
+//			depois descomentar a linha abaixo
+//			g2d.fill(q.getPolygon());
 		}	
 		else if (CASA.equals(textoForma)) 
 		{
 			Graphics2D g2d = (Graphics2D) g.create();
 			g2d.setColor(Color.gray);
 			
-			Point ponto1 = new Point(150, 150);
-			Point ponto2 = new Point(225, 50);
-			Point ponto3 = new Point(300, 150);
-			Point ponto4 = new Point(300, 300);
-			Point ponto5 = new Point(150, 300);
-			
-			Pentagono c = new Pentagono(ponto1, ponto2, ponto3, 
-					ponto4, ponto5);
-			g2d.fill(c.getPolygon());
+//			TODO inicializar pontos, criar pentagono numa variável chamada p, 
+//			depois descomentar a linha abaixo
+//			g2d.fill(p.getPolygon());
 		}
 		else if(PENTAGONO_EQUIL.equals(textoForma)) 
 		{
 			Graphics2D g2d = (Graphics2D) g.create();
 			g2d.setColor(Color.BLUE);
 			
-			Point ponto1 = new Point(350, 150);
-			Point ponto2 = new Point(255, 219);
-			Point ponto3 = new Point(291, 331);
-			Point ponto4 = new Point(409, 331);
-			Point ponto5 = new Point(445, 219);
-			
-			Pentagono c = new Pentagono(ponto1, ponto2, ponto3, 
-					ponto4, ponto5);
-			g2d.fill(c.getPolygon());
+//			TODO inicializar pontos, criar pentagono numa variável chamada p, 
+//			depois descomentar a linha abaixo
+//			g2d.fill(p.getPolygon());
 		}
 	}
 	
